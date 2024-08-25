0a9abb3ad5b0ca1d8238f7c07a907cebdfe360d7
Djordje <cupacdjordje@gmail.com>
2024-08-25 22:10:36 +0200

    admin-login

--- frontend/src/app/admin-login/admin-login.component.ts
+++ frontend/src/app/admin-login/admin-login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+import User from '../models/user';
 
 @Component({
   selector: 'app-admin-login',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-login.component.css']
 })
 export class AdminLoginComponent {
+  constructor(private router: Router, private userService: UserService) { }
 
+  username: string;
+  password: string;
+  error: string;
+
+  login() {
+    if (this.username == "" || this.password == "") {
+      this.error = "Niste uneli sve podatke!";
+      return;
+    }
+
+    else {
+      this.userService.login(this.username, this.password).subscribe((response: {message: string, user: User}) => {
+        if (response.user && response.user.type == 'admin') {
+          if (response.message == 'Login uspešan') {
+            localStorage.setItem("ulogovan", response.user.username);
+            this.router.navigate(['/admin']);
+          } else {
+            this.error = response.message;
+          }
+        } else {
+          this.error = 'Niste autorizovani kao administrator.';
+        }
+      });
+    }
+  }
 }
